5ff540b6ab11994bada24363ce14456298259522
Tales Coelho <tales-coelho@live.com>
2021-06-03 09:25:39 -0300

    criando mais um cachorro

--- dia-1/exercicio-11/class_cachorro.rb
+++ dia-1/exercicio-11/class_cachorro.rb
@@ -19,3 +19,10 @@ puts cachorro1.nome
 puts cachorro1.raca
 cachorro1.latir
 cachorro1.latir("AUUUUUUUU!!")
+
+puts "---------------------------"
+
+cachorro2 = Cachorro.new("Rex", "Pug")
+puts cachorro2.nome
+puts cachorro2.raca
+cachorro2.latir
